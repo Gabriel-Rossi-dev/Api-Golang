2a8eed933b760f3095f7ee2c647d6e7edb59d7be
Gabriel-Rossi-Dev <costagabrielrd@gmail.com>
2024-09-13 14:26:40 -0300

    final commit

--- models/update.go
+++ models/update.go
@@ -9,8 +9,8 @@ func Update(id int64, todo Todo) (int64, error) {
 	}
 	defer conn.Close()
 
-	res, err := conn.Exec(`UPDATE todos SET title =$2, descriptin =$3
-	done =$4 WHERE id =$1`, id, todo.Title, todo.Description, todo.Done)
+	res, err := conn.Exec(`UPDATE todos SET title =$1, description =$2,
+	done =$3 WHERE id =$4 `, todo.Title, todo.Description, todo.Done, id)
 
 	if err != nil {
 		return 0, err
